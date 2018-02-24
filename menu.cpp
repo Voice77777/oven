70c1d543d3b7fe0413c6dbc66ab229cc5f36f46a
Voice Arniman <voice7777777777@gmail.com>
2018-02-24 20:31:38 +0200

    WIP: (Working menu item Difficulty)

--- menu.cpp
+++ menu.cpp
@@ -20,10 +20,12 @@ String menu_items[] = {
 const int menu_menuitem_number = (sizeof(menu_items)/sizeof(menu_items[0]));
 #endif
 
-static int menu_handler_backlight(struct menu_item_t* p, gui_event_t event);
-static int menu_handler_contrast (struct menu_item_t* p, gui_event_t event);
-static int menu_handler_volume   (struct menu_item_t* p, gui_event_t event);
-static int menu_handler_language (struct menu_item_t* p, gui_event_t event);
+static int menu_handler_backlight (struct menu_item_t* p, gui_event_t event);
+static int menu_handler_contrast  (struct menu_item_t* p, gui_event_t event);
+static int menu_handler_volume    (struct menu_item_t* p, gui_event_t event);
+static int menu_handler_language  (struct menu_item_t* p, gui_event_t event);
+static int menu_handler_difficulty(struct menu_item_t* p, gui_event_t event);
+
 
 /* это массив из структур. мы сразу же его инифиализируем нужными значениями. NULL обозначает что мы пока еще
  * не сделали обработчик. и по мере имплементации будем его добавлять
@@ -32,7 +34,7 @@ struct menu_item_t menu_items_[] = {
     { "Contrast",       menu_handler_contrast},
     { "Volume",         menu_handler_volume},
     { "Language",       menu_handler_language},
-    { "Difficulty",     NULL},
+    { "Difficulty",     menu_handler_difficulty},
     { "Light: ON",      menu_handler_backlight},
     { "Reset",          NULL},
 };
@@ -44,6 +46,7 @@ const int menu_language_number = (sizeof(language)/sizeof(language[0]));
 int selectedLanguage = 0;
 
 String difficulty[2] = { "EASY", "HARD" };
+const int menu_difficulty_number = (sizeof(difficulty)/sizeof(difficulty[0]));
 int selectedDifficulty = 0;
 
 void menu_reset_default()
@@ -133,6 +136,33 @@ int menu_handler_language(struct menu_item_t* p, gui_event_t event)
     return 0;
 }
 
+int menu_handler_difficulty(struct menu_item_t* p, gui_event_t event)
+{
+    printf ("callback handler difficulty event: %d\n", event);
+    switch (event) {
+        case GUI_EVENT_SHOW:
+            gui_show_menu_page_string(menu_items[menuitem], difficulty[selectedDifficulty]);
+            break;
+
+        case GUI_EVENT_UP:
+            selectedDifficulty--;
+            if(selectedDifficulty == -1)
+                selectedDifficulty = menu_difficulty_number - 1;
+            break;
+
+        case GUI_EVENT_DOWN:
+            selectedDifficulty++;
+            if(selectedDifficulty == menu_difficulty_number)
+                selectedDifficulty = 0;
+            break;
+
+        default:
+            break;
+    }
+
+    return 0;
+}
+
 void menu_backlight_toggle()
 {
     if (backlight)
@@ -151,20 +181,12 @@ void menu_backlight_toggle()
 
 void menu_show_submenu(int menuitem)
 {
-    switch (menuitem) {
-        case 3: gui_show_menu_page_string(menu_items[menuitem], difficulty[selectedDifficulty]); break;
-    }
+    ;
 }
 
 void menu_update_up_event(int menuitem)
 {
-    if (menuitem==3 ) {
-        selectedDifficulty--;
-        if(selectedDifficulty == -1)
-        {
-            selectedDifficulty = 1;
-        }
-    }
+    ;
 }
 
 int menu_handle_event(int menu_item, gui_event_t event)
@@ -177,13 +199,7 @@ int menu_handle_event(int menu_item, gui_event_t event)
 
 void menu_update_down_event(int menuitem)
 {
-    if (menuitem==3) {
-        selectedDifficulty++;
-        if(selectedDifficulty == 2)
-        {
-            selectedDifficulty = 0;
-        }
-    }
+    ;
 }
 
 void menu_update_click_event(int menuitem)
